35d0e74eae6bb74775485e1226dd66409adc51f3
DmitriiKor <Dmitriikor@gmail.com>
2024-01-21 09:46:28 +0300

    first commit

---
+++ S_n.cpp
@@ -0,0 +1,229 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
+#include <windows.h>
+#include <memory>
+
+const SHORT WIDTH = 20;
+const SHORT HEIGHT = 20;
+
+class Node 
+{
+public:
+    SHORT x, y;
+    SHORT data;
+    std::shared_ptr<Node> next;
+    std::shared_ptr<Node> prev;
+
+    Node(SHORT x, SHORT y, char data = '1') : x(x), y(y), data(data), next(nullptr), prev(nullptr) {}
+};
+
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+void drawField() {
+    for (SHORT i = 0; i < WIDTH + 2; i++) 
+    {
+        std::cout << "#";
+    }
+    std::cout << "\n";
+
+    for (SHORT i = 0; i < HEIGHT; i++) 
+    {
+        std::cout << '#' << std::setw(WIDTH) << std::setfill(' ') << '#' << "\n";
+    }
+
+    for (SHORT i = 0; i < WIDTH + 2; i++) 
+    {
+        std::cout << "#";
+    }
+    std::cout << "\n";
+}
+
+std::shared_ptr<Node> newFruit() 
+{
+    SHORT x = rand() % (WIDTH - 3) + 2;  
+    SHORT y = rand() % (HEIGHT - 3) + 2;  
+    return std::make_shared<Node>(x, y, '0');
+}
+
+class Snake 
+{
+public:
+    std::shared_ptr<Node> head;
+    std::shared_ptr<Node> tail;
+    SHORT length;
+    std::shared_ptr<Node> fruit;
+
+    Snake() : head(new Node(0, 0)), length(1), fruit(newFruit()), tail(head) {}
+
+    char keyScan() 
+    {
+         if (_kbhit()) 
+         {
+            char tempKey = static_cast<char>(_getch());
+            if (tempKey == 'a' || tempKey == 'd' || tempKey == 'w' || tempKey == 's') 
+            {
+                return tempKey;
+            }
+            else 
+            {
+                return 'w';
+            }
+        }
+        else 
+        {
+            return '\0';
+        }
+    }
+
+    void prSHORTList() 
+    {
+        std::shared_ptr<Node> currentNode = head;
+        while (currentNode) 
+        {
+            std::cout << currentNode->data << " ";
+            currentNode = currentNode->next;
+        }
+        std::cout << "\n";
+    }
+
+    void move(char key) 
+    {
+        SHORT x = 0, y = 0;
+        if (key == 'w') 
+        {
+            y = -1;
+        }
+        else if (key == 's') 
+        {
+            y = 1;
+        }
+        else if (key == 'a') 
+        {
+            x = -1;
+        }
+        else if (key == 'd') 
+        {
+            x = 1;
+        }
+
+        SHORT tempX = head->x + x;
+        SHORT tempY = head->y + y;
+
+        if (tempX == fruit->x && tempY == fruit->y) 
+        {
+            Fruit();
+        }
+        else 
+        {
+            if (key != '\0') 
+            {
+                moveTailToBeginning();
+            }
+        }
+
+        head->x += x;
+        head->y += y;
+    }
+
+    void Fruit() 
+    {
+        length++;
+        fruit->data = length;
+        head->prev = fruit;
+        fruit->next = head;
+        head = fruit;
+
+        fruit = newFruit();
+    }
+
+    void moveTailToBeginning() 
+    {
+        if (head == nullptr || tail == nullptr) 
+        {
+            return;
+        }
+
+        if (head == tail) 
+        {
+            return;
+        }
+
+        tail->x = head->x;
+        tail->y = head->y;
+        tail->data = head->data;
+        tail->next = head;
+        head->prev = tail;
+        tail->prev->next = nullptr;
+        tail = tail->prev;
+        head = head->prev;
+        head->prev = nullptr;
+    }
+
+    void displayForward() 
+    {
+        system("cls");
+        drawField();
+        setCursorPosition(fruit->x, fruit->y);
+        std::cout << fruit->data;
+
+        std::shared_ptr<Node> current = head;
+        while (current) 
+        {
+            setCursorPosition(current->x, current->y);
+            std::cout << current->data;
+            current = current->next;
+        }
+
+        Sleep(100);
+    }
+
+    void display() 
+    {
+        std::shared_ptr<Node> current = head;
+        system("cls");
+
+        drawField();
+        setCursorPosition(fruit->x, fruit->y);
+        std::cout << "0";
+
+        while (current) 
+        {
+            setCursorPosition(current->x, current->y);
+            std::cout << current->data;
+            current = current->next;
+        }
+
+        setCursorPosition(0, HEIGHT + 2);
+    }
+
+private:
+    void setCursorPosition(SHORT x, SHORT y) 
+    {
+        COORD coord;
+        coord.X = x;
+        coord.Y = y;
+        SetConsoleCursorPosition(console, coord);
+    }
+};
+
+int main() 
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    Snake snake;
+    while (true) 
+    {
+        snake.displayForward();
+        char key = snake.keyScan();
+        snake.move(key);
+    }
+
+    return 0;
+}
---
+++ S_n.py
@@ -0,0 +1,148 @@
+import msvcrt
+import os
+import time
+import random
+import curses
+
+# Определение размеров поля
+WIDTH, HEIGHT = 20, 20
+stdscr = curses.initscr()
+curses.curs_set(0)
+curses.noecho()
+curses.cbreak()
+
+# Функция для отображения поля
+def draw_field():
+    # Верхняя граница
+    stdscr.addstr(0, 0, "#" * (WIDTH + 2))
+
+    # Боковые границы и пустое пространство внутри
+    for i in range(HEIGHT):
+        stdscr.addstr(1+i, 0, "#" + " " * WIDTH + "#")
+
+    # Нижняя граница
+    stdscr.addstr(HEIGHT + 1, 0, "#" * (WIDTH + 2))
+
+class Node:
+    def __init__(self, x, y, data = '1'):
+        self.x = x
+        self.y = y
+        self.data = data
+        self.next = None
+        self.prev = None
+
+def _new_fruit():
+    new_fruit = Node(random.randint(2, WIDTH-1), random.randint(2, HEIGHT-1), "0")
+    return new_fruit
+
+class Snake:
+    def __init__(self):
+        # Начальная длина змейки и её направление
+        self.head = Node(0, 0)
+        self.length = 1
+        self.new_fruit = _new_fruit()
+        self.tail = self.head
+
+    def key_skan(self):
+        #temp_key = 'w'
+        if msvcrt.kbhit():
+            temp_key = msvcrt.getch()
+            temp_key = temp_key.decode('utf-8') 
+            if (temp_key == 'a') or (temp_key == 'd') or (temp_key == 'w') or (temp_key == 's'):
+                return temp_key
+            else:
+                temp_key = 'w'
+                return None
+        else:
+            return None
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" ")
+            current_node = current_node.next
+        print()
+
+    def move(self, key):
+        x, y = 0, 0
+        if key == 'w':
+            y = -1
+        elif key == 's':
+            y = 1
+        elif key == 'a':
+            x = -1
+        elif key == 'd':
+            x = 1
+        temp_x = self.head.x + x
+        temp_y = self.head.y + y
+        if (((temp_x) == self.new_fruit.x) and ((temp_y) == self.new_fruit.y)):
+            self.frut()
+        else:
+            if key != None:
+                self.move_tail_to_beginning()
+        self.head.x += x
+        self.head.y += y
+
+    def frut (self):
+        self.length += 1
+        self.new_fruit.data = self.length
+        self.head.prev = self.new_fruit
+        self.new_fruit.next = self.head
+        self.head = self.new_fruit
+
+        self.new_fruit = _new_fruit()
+    
+    def move_tail_to_beginning(self):
+        if self.head == None or self.tail == None:
+            return
+        if self.head == self.tail:
+            return
+
+        
+        self.tail.x = self.head.x
+        self.tail.y = self.head.y
+        self.tail.data = self.head.data
+        self.tail.next = self.head
+        self.head.prev = self.tail
+        self.tail.prev.next = None
+        self.tail = self.tail.prev
+        self.head = self.head.prev
+        self.head.prev = None
+
+    def display_forward(self):
+
+        draw_field()
+        stdscr.addch(self.new_fruit.y, self.new_fruit.x, self.new_fruit.data)
+        current = self.head
+        while current:
+            stdscr.addch(current.y, current.x, current.data)
+            stdscr.refresh()
+            current = current.next
+
+    def display(self):
+        current = self.head
+        os.system('cls')
+        
+        draw_field()
+        print(f"\x1b[{self.new_fruit.y};{self.new_fruit.x}H","0")
+        while current:
+            print(f"\x1b[{current.y};{current.x}H", current.data)
+            print("\x1b[0;0H")
+            current = current.next
+            # if current is None:
+            #     print("\x1b[0;0H")
+            # else: 
+            #     print("\x1b[21;22H", end="#")
+
+#-------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------#
+snake = Snake()
+while True:
+    #snake.rule()
+    snake.display_forward()
+    key = snake.key_skan()
+    snake.move(key)
+    time.sleep(0.3)
+
+
