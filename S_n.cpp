1987451a1e046d178ba9f124468a4357d2da1720
DmitriiKor <Dmitriikor@gmail.com>
2024-01-28 01:29:01 +0300

    ADD FOG

--- S_n.cpp
+++ S_n.cpp
@@ -6,6 +6,7 @@
 #include <windows.h>
 #include <memory>
 #include <vector>
+#include <execution>
 
 #define INT_CAST(x) static_cast<int>(x)
 #define SHORT_CAST(x) static_cast<SHORT>(x)
@@ -23,12 +24,18 @@ class Game
 
         Node(SHORT x, SHORT y, char data = '0') : x(x), y(y), data(data), next(nullptr), prev(nullptr) {}
     };
-    
+
     friend class Snake;
-    static const SHORT WIDTH = 10;
-    static const SHORT HEIGHT = 10;
+    static const SHORT WIDTH = 20;
+    static const SHORT HEIGHT = 20;
     std::shared_ptr<Node> fruit;
-    std::vector<std::vector<char>> matrix{HEIGHT + 2, std::vector<char>(WIDTH + 2,' ')};
+    std::shared_ptr<Node> fast;
+    int time = 500;
+    float radius = 1;
+    bool warFog = false;
+    bool isDisplayOld = false;
+    std::vector<std::vector<char>> matrix{HEIGHT + 2, std::vector<char>(WIDTH + 2, ' ')};
+    std::vector<std::vector<char>> fog_Matrix;
 
     enum class Key
     {
@@ -76,52 +83,100 @@ class Game
         for (SHORT i = 0; i < HEIGHT; i++)
         {
             matrix[i][0] = '#';
-            matrix[i][WIDTH+1] = '#';
+            matrix[i][WIDTH + 1] = '#';
         }
 
         matrix[HEIGHT][0] = '#';
-        matrix[HEIGHT][WIDTH+1] = '#';
+        matrix[HEIGHT][WIDTH + 1] = '#';
 
         for (SHORT i = 0; i < WIDTH + 2; i++)
         {
-            matrix[HEIGHT+1][i] = '#';
+            matrix[HEIGHT + 1][i] = '#';
         }
     }
 
     void newFruit()
     {
-        SHORT x = rand() % (WIDTH - 3) + 2;
-        SHORT y = rand() % (HEIGHT - 3) + 2;
-        fruit = std::make_shared<Node>(SHORT_CAST(x), SHORT_CAST(y), '&');
+        for (int i = 0; i <= (WIDTH * HEIGHT) * 20; i++)
+        {
+            SHORT x = rand() % (WIDTH) + 1;
+            SHORT y = rand() % (HEIGHT) + 1;
+            fruit = std::make_shared<Node>(SHORT_CAST(x), SHORT_CAST(y), '&');
+
+            if (matrix[fruit->y][fruit->x] == ' ')
+            {
+                if (matrix[fruit->y][fruit->x] == '=')
+                    std::cout << "\a";
+                matrix[fruit->y][fruit->x] = fruit->data;
+                return;
+                //
+                // for (size_t ii = 0; ii < matrix.size(); ii++)
+                // {
+                //     for (size_t j = 0; j < matrix[ii].size(); j++)
+                //     {
+                //         std::cout << matrix[ii][j];
+                //     }
+                //     std::cout << "\n";
+                // }
+            }
+        }
+        // std::cout << "\n" << i << "\n";
+        throw std::logic_error("newFruit error");
     }
-    
+
+    void newFast()
+    {
+        for (int i = 0; i <= (WIDTH * HEIGHT) * 20; i++)
+        {
+            SHORT x = rand() % (WIDTH) + 1;
+            SHORT y = rand() % (HEIGHT) + 1;
+            fast = std::make_shared<Node>(SHORT_CAST(x), SHORT_CAST(y), 'F');
+            if (matrix[fruit->y][fruit->x] != '=')
+            {
+                matrix[fruit->y][fruit->x] = fast->data;
+                return;
+            }
+        }
+        throw std::logic_error("newFast error");
+    }
+
     std::shared_ptr<Node> getFruit()
     {
         return fruit;
     }
 
+public:
+    Game()
+    {
+        newFruit();
+        newFast();
+        drawFieldMtrx();
+    }
 
-    public:
-        Game() 
-        {
-            newFruit();
-            drawFieldMtrx();
-        }
+    void WarFog(bool isFog)
+    {
+        warFog = isFog;
+    }
 
+    void displayOld(bool is_DisplayOld)
+    {
+        isDisplayOld = is_DisplayOld;
+    }
     class Snake
     {
         friend class Game;
+
     public:
         std::shared_ptr<Node> head;
         std::shared_ptr<Node> tail;
         SHORT length;
-        Game& game;
+        Game &game;
         int Pressed_Key = INT_CAST(Key::w);
 
-        Snake(Game& game) : game(game), head(std::make_shared<Node>(SHORT_CAST(WIDTH / 2), SHORT_CAST(HEIGHT / 2), '|'))
+        Snake(Game &game) : game(game), head(std::make_shared<Node>(SHORT_CAST(WIDTH / 2), SHORT_CAST(HEIGHT / 2), '|'))
         {
-          game.matrix[head->y][head->x] = head->data;   
-          tail = head;  
+            game.matrix[head->y][head->x] = head->data;
+            tail = head;
         }
 
         void keyScan()
@@ -137,7 +192,7 @@ class Game
             else
                 return;
         }
-        //TODO del this
+        // TODO del this
         void printList()
         {
             std::shared_ptr<Node> currentNode = head;
@@ -163,37 +218,54 @@ class Game
 
             SHORT tempX = head->x + x;
             SHORT tempY = head->y + y;
-            
+
             game.matrix[tail->y][tail->x] = ' ';
 
-            if (tempX ==  game.fruit->x && tempY ==  game.fruit->y)
+            if (tempX == game.fruit->x && tempY == game.fruit->y)
             {
-                Fruit_eat();
+                fruit_Eat();
                 game.matrix[tail->y][tail->x] = ';';
                 game.matrix[head->y][head->x] = '=';
                 return;
             }
+            if (tempX == game.fast->x && tempY == game.fast->y)
+            {
+                game.matrix[game.fast->y][game.fast->x] = ' ';
+                fastEat();
+            }
 
             moveTailToBeginning();
-            checkRule(x,y);
+
+            checkRule(x, y);
+
             head->x += x;
             head->y += y;
 
+            game.matrix[game.fast->y][game.fast->x] = game.fast->data;
             game.matrix[tail->y][tail->x] = ';';
             game.matrix[head->y][head->x] = '=';
             game.matrix[game.fruit->y][game.fruit->x] = game.fruit->data;
         }
 
-        void Fruit_eat()
+        void fruit_Eat()
         {
             length++;
             // fruit->data = static_cast<char>(length+48);
             game.fruit->data = '|';
-            head->prev =  game.fruit;
+            head->prev = game.fruit;
             game.fruit->next = head;
             head = game.fruit;
 
             game.newFruit();
+        }
+
+        void fastEat()
+        {
+            game.time -= 25;
+            game.radius += 0.500; // ceil(((game.radius + (float)1.5)/(float)1.4)*(float)1000)/(float)1000;
+            if (game.radius > HEIGHT / 2)
+                game.radius = HEIGHT / 2;
+            game.newFast();
         }
 
         void moveTailToBeginning()
@@ -215,55 +287,75 @@ class Game
             head = head->prev;
             head->prev = nullptr;
         }
-        //TODO move to Game
+
         void displayForward()
         {
             system("cls");
-            game.drawField();
-            setCursorPosition( game.fruit->x,  game.fruit->y);
-            std::cout <<  game.fruit->data;
-
-            std::shared_ptr<Node> current = head;
-
-            while (current)
+            if (game.isDisplayOld)
             {
-                setCursorPosition(current->x, current->y);
-                if (current == head)
-                    std::cout << '=';
-                else if (current != tail)
+                game.drawField();
+                setCursorPosition(game.fruit->x, game.fruit->y);
+                std::cout << game.fruit->data;
+                setCursorPosition(game.fast->x, game.fast->y);
+                std::cout << game.fast->data;
+                std::shared_ptr<Node> current = head;
+                setCursorPosition(0, 0);
+                std::cout << game.radius;
+
+                while (current)
                 {
-                    if ((Pressed_Key == INT_CAST(Key::a)) || (Pressed_Key == INT_CAST(Key::d)))
-                        std::cout << '-';
+                    setCursorPosition(current->x, current->y);
+                    if (current == head)
+                        std::cout << '=';
+                    else if (current != tail)
+                    {
+                        if ((Pressed_Key == INT_CAST(Key::a)) || (Pressed_Key == INT_CAST(Key::d)))
+                            std::cout << '-';
+                        else
+                            std::cout << current->data;
+                    }
                     else
-                        std::cout << current->data;
+                        std::cout << ';';
+                    current = current->next;
+                }
+                setCursorPosition(head->x, head->y);
+            }
+            else
+            { // setCursorPosition(0, game.HEIGHT + 3);
+                if (!game.warFog)
+                {
+                    for (size_t i = 0; i < game.matrix.size(); i++)
+                    {
+                        for (size_t j = 0; j < game.matrix[i].size(); j++)
+                        {
+                            std::cout << game.matrix[i][j];
+                        }
+                        std::cout << "\n";
+                    }
                 }
                 else
-                    std::cout << ';';
-                current = current->next;
-            }
-            setCursorPosition(head->x, head->y);
-
-            setCursorPosition(0, game.HEIGHT+3);
-            for (size_t i = 0; i < game.matrix.size(); i++)
-            {
-                for (size_t j = 0; j < game.matrix[i].size(); j++)
                 {
-                    std::cout << game.matrix[i][j];
-                    
+                    WarFog();
+                    for (size_t i = 0; i < game.fog_Matrix.size(); i++)
+                    {
+                        for (size_t j = 0; j < game.fog_Matrix[i].size(); j++)
+                        {
+                            std::cout << game.fog_Matrix[i][j];
+                        }
+                        std::cout << "\n";
+                    }
                 }
-                std::cout << "\n";
             }
-
-            Sleep(300);
+            Sleep(game.time);
         }
-        //TODO move to Game
+
         void display()
         {
             std::shared_ptr<Node> current = head;
             system("cls");
 
             game.drawField();
-            setCursorPosition( game.fruit->x,  game.fruit->y);
+            setCursorPosition(game.fruit->x, game.fruit->y);
             std::cout << "0";
 
             while (current)
@@ -278,10 +370,126 @@ class Game
 
         void checkRule(SHORT x, SHORT y)
         {
-            if(game.matrix[head->y+y][head->x+x] != ' ')
+            if (game.matrix[head->y + y][head->x + x] != ' ')
             {
                 std::cout << "\a";
+                setCursorPosition(30, 30);
+                system("pause");
+                system("pause");
                 exit(-9);
+            }
+        }
+
+        std::vector<std::vector<char>> WarFog()
+        {
+            game.fog_Matrix = game.matrix;
+            drawCircle(game.fog_Matrix, head->y, head->x, INT_CAST(game.radius), ':');
+
+            // for (size_t i = 0; i < game.fog_Matrix.size(); ++i)
+            // {
+            //     for (size_t j = 0; j < game.fog_Matrix[i].size(); ++j)
+            //     {
+            //         std::cout << game.fog_Matrix[i][j];
+            //     }
+            //     std::cout << "\n";
+            // }
+
+            for (size_t i = 0; i < game.fog_Matrix.size(); ++i)
+            {
+                for (size_t j = 0; j < game.fog_Matrix[i].size(); ++j)
+                {
+                    if (game.fog_Matrix[i][j] == ':')
+                    {
+                        game.fog_Matrix[i][j] = game.matrix[i][j];
+                    }
+                    else
+                    {
+                        game.fog_Matrix[i][j] = '~';
+                    }
+                }
+            }
+
+            return game.fog_Matrix;
+        }
+
+        void drawCircle(std::vector<std::vector<char>> &matrix, int centerX, int centerY, int radius, char symbol)
+        {
+            int x = radius;
+            int y = 0;
+            int err = 0;
+
+            while (x >= y)
+            {
+                {
+                    int i, lim;
+                    if (centerX - x < 0)
+                        i = 0;
+                    else
+                        i = centerX - x;
+
+                    if (centerX + x >= INT_CAST(matrix.size()) - 1)
+                        lim = matrix.size() - 1;
+                    else
+                        lim = centerX + x;
+
+                    int limY, Lim_Y;
+                    if (centerY + y >= INT_CAST(matrix.size()) - 1)
+                        limY = matrix.size() - 1;
+                    else
+                        limY = centerY + y;
+
+                    if (centerY - y < 0)
+                        Lim_Y = 0;
+                    else
+                        Lim_Y = centerY - y;
+
+                    for (; i <= lim; ++i)
+                    {
+                        matrix[i][limY] = symbol;
+                        matrix[i][Lim_Y] = symbol;
+                    }
+                }
+                {
+                    int i, lim;
+                    if (centerX - y < 0)
+                        i = 0;
+                    else
+                        i = centerX - y;
+
+                    if (centerX + y >= INT_CAST(matrix.size()) - 1)
+                        lim = matrix.size() - 1;
+                    else
+                        lim = centerX + y;
+
+                    int limY, Lim_Y;
+                    if (centerY + x >= INT_CAST(matrix.size()) - 1)
+                        limY = matrix.size() - 1;
+                    else
+                        limY = centerY + x;
+
+                    if (centerY - x < 0)
+                        Lim_Y = 0;
+                    else
+                        Lim_Y = centerY - x;
+
+                    for (; i <= lim; ++i)
+                    {
+                        matrix[i][limY] = symbol;
+                        matrix[i][Lim_Y] = symbol;
+                    }
+                }
+
+                if (err <= 0)
+                {
+                    y += 1;
+                    err += 2 * y + 1;
+                }
+
+                if (err > 0)
+                {
+                    x -= 1;
+                    err -= 2 * x + 1;
+                }
             }
         }
 
@@ -309,10 +517,11 @@ int main()
     {
         srand(static_cast<unsigned>(time(nullptr)));
 
-        //TODO WarFog add
+        game.displayOld(false);
         snake.displayForward();
         snake.keyScan();
         snake.move();
+        game.WarFog(true);
     }
 
     return 0;
