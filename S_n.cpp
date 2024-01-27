fc44f622f807e5d822cd884c0555bac57aebd78c
DmitriiKor <Dmitriikor@gmail.com>
2024-01-27 18:15:37 +0300

    Class Game added

--- S_n.cpp
+++ S_n.cpp
@@ -5,232 +5,254 @@
 #include <conio.h>
 #include <windows.h>
 #include <memory>
+#include <vector>
 
 #define INT_CAST(x) static_cast<int>(x)
 #define SHORT_CAST(x) static_cast<SHORT>(x)
-
-const SHORT WIDTH = 20;
-const SHORT HEIGHT = 20;
-
-enum class Key {w = 'w', a = 'a', d = 'd', s = 's', ESC = 27};
-enum class Direction {UP = Key::w, DOWN = Key::s, LEFT = Key::a, RIGHT = Key::d};
-class Node 
-{
-public:
-    SHORT x, y;
-    char data;
-    std::shared_ptr<Node> next;
-    std::shared_ptr<Node> prev;
-
-    Node(SHORT x, SHORT y, char data = '0') : x(x), y(y), data(data), next(nullptr), prev(nullptr) {}
-};
-
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 
-void drawField() 
+class Game
 {
-    for (SHORT i = 0; i < WIDTH + 2; i++) 
+    class Node
     {
-        std::cout << "#";
-    }
-    std::cout << "\n";
+    public:
+        SHORT x, y;
+        char data;
+        std::shared_ptr<Node> next;
+        std::shared_ptr<Node> prev;
 
-    for (SHORT i = 0; i < HEIGHT; i++) 
-    {
-        std::cout << '#' << std::setw(WIDTH+1) << std::setfill(' ') << '#' << "\n";
-    }
-
-    for (SHORT i = 0; i < WIDTH + 2; i++) 
-    {
-        std::cout << "#";
-    }
-    std::cout << "\n";
-}
-
-std::shared_ptr<Node> newFruit() 
-{
-    SHORT x = rand() % (WIDTH - 3) + 2;  
-    SHORT y = rand() % (HEIGHT - 3) + 2;  
-    return std::make_shared<Node>(SHORT_CAST(x), SHORT_CAST(y), '&');
-}
-
-class Snake 
-{
-public:
-    std::shared_ptr<Node> head;
-    std::shared_ptr<Node> tail;
-    SHORT length;
-    bool is_new_pressed = false;
-    int Pressed_Key = INT_CAST(Key::w);
+        Node(SHORT x, SHORT y, char data = '0') : x(x), y(y), data(data), next(nullptr), prev(nullptr) {}
+    };
+    
+    friend class Snake;
+    static const SHORT WIDTH = 20;
+    static const SHORT HEIGHT = 20;
     std::shared_ptr<Node> fruit;
+    std::vector<std::vector<char>> matrix{WIDTH + 2, std::vector<char>(HEIGHT + 2, '~')};
 
-    Snake() : head(new Node(SHORT_CAST(10), SHORT_CAST(10), '|')), length(1), fruit(newFruit()), tail(head) {}
-
-    void keyScan() 
+    enum class Key
     {
-         if (_kbhit()) 
-         {
-            int tempKey = _getch();
-            if (tempKey == INT_CAST(Key::a) || tempKey == INT_CAST(Key::d)
-               || tempKey == INT_CAST(Key::w) || tempKey == INT_CAST(Key::s)) 
-            {
-                Pressed_Key = tempKey;
-                is_new_pressed = true;
-            }
-            else 
-            {
-                return;
-            }
-        }
-        else 
-        {
-            return;
-        }
-    }
-
-    void printList() 
+        w = 'w',
+        a = 'a',
+        d = 'd',
+        s = 's',
+        ESC = 27
+    };
+    enum class Direction
     {
-        std::shared_ptr<Node> currentNode = head;
-        while (currentNode) 
+        UP = Key::w,
+        DOWN = Key::s,
+        LEFT = Key::a,
+        RIGHT = Key::d
+    };
+    static void drawField()
+    {
+        for (SHORT i = 0; i < WIDTH + 2; i++)
         {
-            std::cout << currentNode->data << " ";
-            currentNode = currentNode->next;
+            std::cout << "#";
+        }
+        std::cout << "\n";
+
+        for (SHORT i = 0; i < HEIGHT; i++)
+        {
+            std::cout << '#' << std::setw(WIDTH + 1) << std::setfill(' ') << '#' << "\n";
+        }
+
+        for (SHORT i = 0; i < WIDTH + 2; i++)
+        {
+            std::cout << "#";
         }
         std::cout << "\n";
     }
 
-    void move() 
+    void newFruit()
     {
-        SHORT x = 0, y = 0;
-        if (Pressed_Key == INT_CAST(Direction::UP)) 
-            y = -1;
-        else if (Pressed_Key == INT_CAST(Direction::DOWN) )
-            y = 1;
-        else if (Pressed_Key == INT_CAST(Direction::LEFT) )
-            x = -1;
-        else if (Pressed_Key == INT_CAST(Direction::RIGHT)) 
-            x = 1;
+        SHORT x = rand() % (WIDTH - 3) + 2;
+        SHORT y = rand() % (HEIGHT - 3) + 2;
+        fruit = std::make_shared<Node>(SHORT_CAST(x), SHORT_CAST(y), '&');
+    }
+    
+    std::shared_ptr<Node> getFruit()
+    {
+        return fruit;
+    }
 
-        SHORT tempX = head->x + x;
-        SHORT tempY = head->y + y;
-
-        if (tempX == fruit->x && tempY == fruit->y) 
+    public:
+        Game() 
         {
-            Fruit();
-            return;
+            newFruit();
         }
 
-        moveTailToBeginning();
-
-        head->x += x;
-        head->y += y;
-    }
-
-    void Fruit() 
+    class Snake
     {
-        length++;
-        //fruit->data = static_cast<char>(length+48);
-        fruit->data = '|';
-        head->prev = fruit;
-        fruit->next = head;
-        head = fruit;
+        friend class Game;
+    public:
+        std::shared_ptr<Node> head;
+        std::shared_ptr<Node> tail;
+        SHORT length;
 
-        fruit = newFruit();
-    }
+        int Pressed_Key = INT_CAST(Key::w);
 
-    void moveTailToBeginning() 
-    {
-        if (head == nullptr || tail == nullptr) 
-            return;
+        Snake( ) : head(std::make_shared<Node>(SHORT_CAST(WIDTH / 2), SHORT_CAST(HEIGHT / 2), '|')), length(1), tail(head) {}
 
-        if (head == tail) 
-            return;
-
-        tail->x = head->x;
-        tail->y = head->y;
-        tail->data = head->data;
-        tail->next = head;
-        head->prev = tail;
-        tail->prev->next = nullptr;
-        tail = tail->prev;
-        // 
-        head = head->prev;
-        head->prev = nullptr;
-    }
-
-    void displayForward() 
-    {
-        system("cls");
-        drawField();
-        setCursorPosition(fruit->x, fruit->y);
-        std::cout << fruit->data;
-
-        std::shared_ptr<Node> current = head;
-        
-        while (current) 
+        void keyScan()
         {
-            setCursorPosition(current->x, current->y);
-            if (current == head)
-                std::cout << '=';
-            else if (current!=tail)
+            if (_kbhit())
             {
-                if((Pressed_Key == INT_CAST(Key::a)) || (Pressed_Key == INT_CAST(Key::d)))
-                     std::cout << '-';
-                else     
-                    std::cout << current->data;
+                int tempKey = _getch();
+                if (tempKey == INT_CAST(Key::a) || tempKey == INT_CAST(Key::d) || tempKey == INT_CAST(Key::w) || tempKey == INT_CAST(Key::s))
+                    Pressed_Key = tempKey;
+                else
+                    return;
             }
             else
-                std::cout << ';';
-            current = current->next;
+                return;
         }
-        setCursorPosition(head->x, head->y);
-        
-        Sleep(300);
-    }
 
-    void display() 
-    {
-        std::shared_ptr<Node> current = head;
-        system("cls");
-
-        drawField();
-        setCursorPosition(fruit->x, fruit->y);
-        std::cout << "0";
-
-        while (current) 
+        void printList()
         {
-            setCursorPosition(current->x, current->y);
-            std::cout << current->data;
-            current = current->next;
+            std::shared_ptr<Node> currentNode = head;
+            while (currentNode)
+            {
+                std::cout << currentNode->data << " ";
+                currentNode = currentNode->next;
+            }
+            std::cout << "\n";
         }
 
-        setCursorPosition(0, HEIGHT + 2);
-    }
+        void move(Game& game)
+        {
+            SHORT x = 0, y = 0;
+            if (Pressed_Key == INT_CAST(Direction::UP))
+                y = -1;
+            else if (Pressed_Key == INT_CAST(Direction::DOWN))
+                y = 1;
+            else if (Pressed_Key == INT_CAST(Direction::LEFT))
+                x = -1;
+            else if (Pressed_Key == INT_CAST(Direction::RIGHT))
+                x = 1;
 
-private:
-    void setCursorPosition(SHORT x, SHORT y) 
-    {
-        COORD coord;
-        coord.X = x;
-        coord.Y = y;
-        SetConsoleCursorPosition(console, coord);
-    }
+            SHORT tempX = head->x + x;
+            SHORT tempY = head->y + y;
+            
+            if (tempX ==  game.fruit->x && tempY ==  game.fruit->y)
+            {
+                Fruit_eat(game);
+                return;
+            }
+
+            moveTailToBeginning();
+
+            head->x += x;
+            head->y += y;
+        }
+
+        void Fruit_eat(Game& game)
+        {
+            length++;
+            // fruit->data = static_cast<char>(length+48);
+            game.fruit->data = '|';
+            head->prev =  game.fruit;
+            game.fruit->next = head;
+            head = game.fruit;
+
+            game.newFruit();
+        }
+
+        void moveTailToBeginning()
+        {
+            if (head == nullptr || tail == nullptr)
+                return;
+
+            if (head == tail)
+                return;
+
+            tail->x = head->x;
+            tail->y = head->y;
+            tail->data = head->data;
+            tail->next = head;
+            head->prev = tail;
+            tail->prev->next = nullptr;
+            tail = tail->prev;
+            //
+            head = head->prev;
+            head->prev = nullptr;
+        }
+
+        void displayForward(Game& game)
+        {
+            system("cls");
+            drawField();
+            setCursorPosition( game.fruit->x,  game.fruit->y);
+            std::cout <<  game.fruit->data;
+
+            std::shared_ptr<Node> current = head;
+
+            while (current)
+            {
+                setCursorPosition(current->x, current->y);
+                if (current == head)
+                    std::cout << '=';
+                else if (current != tail)
+                {
+                    if ((Pressed_Key == INT_CAST(Key::a)) || (Pressed_Key == INT_CAST(Key::d)))
+                        std::cout << '-';
+                    else
+                        std::cout << current->data;
+                }
+                else
+                    std::cout << ';';
+                current = current->next;
+            }
+            setCursorPosition(head->x, head->y);
+
+            Sleep(300);
+        }
+
+        void display(Game& game)
+        {
+            std::shared_ptr<Node> current = head;
+            system("cls");
+
+            drawField();
+            setCursorPosition( game.fruit->x,  game.fruit->y);
+            std::cout << "0";
+
+            while (current)
+            {
+                setCursorPosition(current->x, current->y);
+                std::cout << current->data;
+                current = current->next;
+            }
+
+            setCursorPosition(0, HEIGHT + 2);
+        }
+
+    private:
+        void setCursorPosition(SHORT x, SHORT y)
+        {
+            COORD coord;
+            coord.X = x;
+            coord.Y = y;
+            SetConsoleCursorPosition(console, coord);
+        }
+    };
 };
 
-int main() 
+int main()
 {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
+    Game game;
+    Game::Snake snake;
 
-
-    Snake snake;
-    while (true) 
+    while (true)
     {
         srand(static_cast<unsigned>(time(nullptr)));
-        snake.displayForward();
+        snake.displayForward(game);
         snake.keyScan();
-        snake.move();
+        snake.move(game);
     }
 
     return 0;
